5a1dfe4c010df3114aec08dbc7f79bc1c0546bd0
matheus0000 <linuxmatheus@outlook.com>
2020-08-08 18:49:48 -0300

    New

---
+++ PYthon/Hello World.py
@@ -0,0 +1,21 @@
+a = 'Hello World -- Inglés'
+
+print(a)
+
+b = 'Olá Mundo -- Português'
+
+print(b)
+
+c = 'Hola Mundo -- Espanhol'
+
+print(c)
+
+d = 'Привет мир -- Russo'
+
+print(d)
+
+
+e = 'Hallo Welt -- Alemão'
+
+print(e)
+
