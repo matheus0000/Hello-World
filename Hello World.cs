b09739fb7eddbb52ac01f8c7a957a499427a8fbd
matheus0000 <linuxmatheus@outlook.com>
2020-08-08 19:23:18 -0300

    New

---
+++ Hello World.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!" );
+            Console.ReadKey(); 
+            Console.ReadLine();
+        }
+    }
+}
