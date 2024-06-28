53a45c81f1b7291c7892f050a91197250dd68bf9
matheus0000 <linuxmatheus@outlook.com>
2024-06-28 06:10:28 -0300

    0.2

--- Python/Hello World.py
+++ Python/Hello World.py
@@ -1,21 +1,12 @@
 a = 'Hello World -- Inglés'
-
-print(a)
-
 b = 'Olá Mundo -- Português'
-
-print(b)
-
 c = 'Hola Mundo -- Espanhol'
-
-print(c)
-
 d = 'Привет мир -- Russo'
-
-print(d)
-
-
 e = 'Hallo Welt -- Alemão'
 
+print(a)
+print(b)
+print(c)
+print(d)
 print(e)
 
