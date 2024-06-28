f7b48019e89b3ea3db30f44b757607a3b3e48e8c
matheus0000 <linuxmatheus@outlook.com>
2024-06-28 06:16:16 -0300

    0.3

--- Python/Hello World.py
+++ Python/Hello World.py
@@ -1,3 +1,5 @@
+# Definindo as mensagens em diferentes idiomas
+
 a = 'Hello World -- Inglés'
 b = 'Olá Mundo -- Português'
 c = 'Hola Mundo -- Espanhol'
@@ -9,4 +11,19 @@ print(b)
 print(c)
 print(d)
 print(e)
+print('#####')
+
+# Definindo as mensagens em diferentes idiomas
+mensagens = [
+    'Hello World -- Inglês',
+    'Olá Mundo -- Português',
+    'Hola Mundo -- Espanhol',
+    'Привет мир -- Russo',
+    'Hallo Welt -- Alemão'
+]
+
+# Iterando sobre a lista e imprimindo cada mensagem
+for mensagem in mensagens:
+    print(mensagem)
+
 
